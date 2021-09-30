ec24f02c7b2260134f9cbf225c169ec67ad99bf8
Brandon Leyva <90630618+BrandonLeyva@users.noreply.github.com>
2021-09-30 01:05:26 -0500

    1

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,67 @@
+const UserService = require("../services/users.service")
+
+const getAllUsers = async (req, res, next) => {
+    try{
+        const users = await UserService.getAll()
+        res.json(users)
+    }catch(err){
+        next(err)
+    }
+}
+
+const getUserById = async (req, res, next) => {
+    try{
+        const {id} = req.params
+        const user = await UserService.getById(id)
+        res.json(user)
+    }catch(err){
+        next(err)
+    }
+}
+
+const createUser = async (req, res, next) => {
+    try{
+        const {firstname, lastname, email, password, profile_image, phone} = req.body
+        
+        const newUser = {
+            firstname, 
+            lastname,
+            email,
+            password,
+            profile_image: profile_image || null,
+            phone
+        }
+        
+        const user = await UserService.create(newUser)
+        res.status(201).json(user)
+    }catch(err){
+        next(err)
+    }
+}
+
+const updateUser = async (req, res, next) => {
+    try{
+        const user = await UserService.update(req.body, req.params.id)
+        res.json(user)
+    }catch(err){
+        next(err)
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try{
+        const {id} = req.params
+        const user = await UserService.delete(id)
+        res.json(user)
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+}
---
+++ src/services/users.service.js
@@ -0,0 +1,56 @@
+const {users} = require("../models")
+
+
+class UserService{
+    static async getAll(){
+        try{
+            console.log(users)
+            let results = await users.findAll()
+            return results
+        }catch(err){
+            throw err
+        }
+    }
+
+    static async getById(id){
+        try{
+            let result = await users.findByPk(id)
+            if(result)
+                return result
+            return {}
+        }catch(err){
+            throw err
+        }
+    }
+
+    static async create(newUser){
+        try{
+            let result = await users.create(newUser)
+            return result
+        }catch(err){
+            throw err
+        }
+    }
+
+    static async update(updateUser, id){
+        try{
+            let result = await users.update(updateUser, {where: {id}})
+            return result
+        }catch(err){
+            throw err
+        }
+    }
+
+    static async delete(id){
+        try{
+            let result = await users.destroy({where: {id}})
+            if(result === 1)
+                return `Se eliminó el usuario con el id ${id} de la base de datos`
+            return `No existe un usuario con id ${id} en la base de datos`
+        }catch(err){
+            throw err
+        }
+    }
+}
+
+module.exports = UserService
