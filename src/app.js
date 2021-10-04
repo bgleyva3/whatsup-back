6461ae9184cd2a81e597d5600bb1d76b25289d84
Brandon Leyva <90630618+BrandonLeyva@users.noreply.github.com>
2021-10-03 23:30:19 -0500

    6 documentation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const userRoutes = require("./routes/users.routes")
 const conversationRoutes = require("./routes/conversations.routes")
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require("../src/swagger.json")
+
 const app = express()
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.json())
 app.use("/api/v1", (req, res, next) => {console.log(req.body); next()})
