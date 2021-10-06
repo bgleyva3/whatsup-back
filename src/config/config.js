ed75e93e2c9ed8c725487783dbe51e72446db4a2
Brandon Leyva <90630618+BrandonLeyva@users.noreply.github.com>
2021-10-06 00:07:36 -0500

    10

--- src/config/config.js
+++ src/config/config.js
@@ -16,10 +16,12 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
+    use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        required: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
